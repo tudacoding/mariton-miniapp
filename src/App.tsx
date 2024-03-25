3fbcf577de61e5c1ec5d71f6dec70ef7d9d60ec3
Thinh Nguyen <thinhnguyen.teamx@gmail.com>
2024-03-25 21:11:40 +0700

    add twa-dev

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import CounterApp from "./modules/Counter";
+import '@twa-dev/sdk';
+
 
 function App() {
   return (
