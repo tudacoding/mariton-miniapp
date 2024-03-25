228fc08a4a877d539e4c178a32d4199b0aa38b95
Thinh Nguyen <thinhnguyen.teamx@gmail.com>
2024-03-25 17:46:11 +0700

    update manifest

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 // this manifest is used temporarily for development purposes
 const manifestUrl =
-  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+  "https://ton-blockchain-hello-world.vercel.app/public/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
