3af403ff31ea4668e962fa715ab40f26e22c3402
Thai Bui Minh <=>
2024-06-14 01:04:35 +0700

    continue

--- src/pages/airdrop-home.tsx
+++ src/pages/airdrop-home.tsx
@@ -1,7 +1,7 @@
 import HomeLayout from "@/modules/home/Layout";
 import background from "@/assets/air/background-body-short.png";
-// import mainBanner from "@/assets/air/background-homefix.gif";
-import mainBanner from "@/assets/air/mining-animation.gif";
+import mainBanner from "@/assets/air/background-homefix.gif";
+// import mainBanner from "@/assets/air/mining-animation.gif";
 import btnMaritonTK from "@/assets/air/mariton-tk-ico.png";
 import { useDispatch } from "react-redux";
 import { Dispatch } from "@/store/store";
@@ -17,74 +17,74 @@ export default function AirDopHome() {
   return (
     <HomeLayout>
       <div className="h-full flex flex-col">
-        <div className="grow overflow-auto">
-          <div className="w-full h-fix relative">
-            <img
-              className="w-full h-full absolute z-[-10]"
-              src={background}
-              alt="bg-mission-body"
-            />
-            <div className="p-6 w-full h-full">
-              <div className="relative rounded-2xl overflow-hidden w-full">
-                <img className="w-full h-full" src={mainBanner} />
-                <span className="absolute top-0 text-t-dark font-semibold text-xs my-1.5 mx-2 bg-black/10 px-1 rounded-2xl ">
-                  CURRENT SPEED 0.1 MRT/H
+        <div className="grow overflow-auto w-full h-full relative">
+          {/* <div className="w-full h-fix relative"> */}
+          <img
+            className="w-full h-full absolute z-[-10]"
+            src={background}
+            alt="bg-mission-body"
+          />
+          <div className="p-6 w-full h-full flex flex-col">
+            <div className="rounded-2xl overflow-hidden grow relative flex justify-center items-center">
+              <img className="w-full h-full object-cover" src={mainBanner} />
+              <span className="absolute top-0 left-0 text-t-dark font-semibold text-xs my-1.5 mx-2 bg-black/10 px-1 rounded-2xl ">
+                CURRENT SPEED 0.1 MRT/H
+              </span>
+            </div>
+            <div className="grid grid-cols-2 gap-3 my-3">
+              <BaseButton
+                className={"text-lg font-bold !text-t-button !bg-card"}
+                onClick={() => {
+                  handleDialog({
+                    isVisible: true,
+                    children: <LeaderboardDialog />,
+                    classWrapperDialog: "!p-0 !overflow-visible",
+                    classDialog: "top-[-20px]",
+                  });
+                }}
+              >
+                Leaderboard
+              </BaseButton>
+              <BaseButton
+                className={"text-lg font-bold !text-t-button !bg-card"}
+                onClick={() => {
+                  // handleDialog({
+                  //   isVisible: true,
+                  //   children: <ClaimDialog />,
+                  //   classWrapperDialog: "!p-0 !overflow-visible",
+                  // });
+                }}
+              >
+                Claim
+              </BaseButton>
+            </div>
+            <div className="flex flex-col">
+              <p className="flex flex-row justify-center gap-1 pb-2">
+                <span className="font-bold text-2xl text-t-button">
+                  {amount}
                 </span>
-              </div>
-              <div className="grid grid-cols-2 gap-3 my-3">
+                <img src={btnMaritonTK} alt="" className="object-contain" />
+              </p>
+              <div className="text-center">
                 <BaseButton
-                  className={"text-lg font-bold !text-t-button !bg-card"}
                   onClick={() => {
                     handleDialog({
                       isVisible: true,
-                      children: <LeaderboardDialog />,
+                      children: <LevelUpDialog />,
                       classWrapperDialog: "!p-0 !overflow-visible",
-                      classDialog: "top-[-20px]",
                     });
                   }}
+                  className="!bg-base !rounded-full text-white font-bold text-[22px] !pb-3 leading-none w-[200px]"
                 >
-                  Leaderboard
-                </BaseButton>
-                <BaseButton
-                  className={"text-lg font-bold !text-t-button !bg-card"}
-                  onClick={() => {
-                    // handleDialog({
-                    //   isVisible: true,
-                    //   children: <ClaimDialog />,
-                    //   classWrapperDialog: "!p-0 !overflow-visible",
-                    // });
-                  }}
-                >
-                  Claim
+                  Level 2
                 </BaseButton>
               </div>
-              <div className="flex flex-col">
-                <p className="flex flex-row justify-center gap-1 pb-2">
-                  <span className="font-bold text-2xl text-t-button">
-                    {amount}
-                  </span>
-                  <img src={btnMaritonTK} alt="" className="object-contain" />
-                </p>
-                <div className="text-center">
-                  <BaseButton
-                    onClick={() => {
-                      handleDialog({
-                        isVisible: true,
-                        children: <LevelUpDialog />,
-                        classWrapperDialog: "!p-0 !overflow-visible",
-                      });
-                    }}
-                    className="!bg-base !rounded-full text-white font-bold text-[22px] !pb-3 leading-none w-[200px]"
-                  >
-                    Level 2
-                  </BaseButton>
-                </div>
-              </div>
-              <p className="py-3 text-t-dark text-xs font-semibold text-center whitespace-nowrap">
-                COST 0.5 MRT NEWSPEED 0.5 MRT/H
-              </p>
             </div>
+            <p className="py-3 text-t-dark text-xs font-semibold text-center whitespace-nowrap">
+              COST 0.5 MRT NEWSPEED 0.5 MRT/H
+            </p>
           </div>
+          {/* </div> */}
         </div>
       </div>
     </HomeLayout>
