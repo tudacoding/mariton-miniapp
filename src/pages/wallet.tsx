c97a79ca80fa8669ca7a8abfc4777ca7e0baf6e0
Thai Bui Minh <=>
2024-07-17 22:47:08 +0700

    deposit

--- src/pages/wallet.tsx
+++ src/pages/wallet.tsx
@@ -7,13 +7,15 @@ import { Dispatch, RootState } from "@/store/store";
 import { useMaritonToken } from "@/hooks/useMaritonToken";
 // import mrtPng from "@/assets/air/mariton-tk-ico.png";
 // import tonPng from "@/assets/game/lottery-item/ton.png";
-import { beginCell } from "@ton/core";
+import { Address, beginCell, toNano } from "@ton/core";
 import { ClaimMRT } from "@/contract/claim";
 import { useNavigate } from "react-router-dom";
 import HomeLayout from "@/modules/home/Layout";
 import BaseInput from "@/components/BaseInput";
 import { useState } from "react";
-
+import { useTonConnectUI } from "@tonconnect/ui-react";
+const DEPOSIT_WALLET = "0QAszBVzU37ZyRzA0I5Dn-RZoY6qg2FtGWN4Q356vsR_3jX_";
+const addressMrt = "EQBAK2GFaOix6p9rRP2URa2Uf8Th8XvzuymnFPPycyUAGvCp";
 // function CardToken({ title, ton = 0.0, mrt = 0.0 }: any) {
 //   return (
 //     <div className="w-full">
@@ -55,8 +57,13 @@ import { useState } from "react";
 //   );
 // }
 export default function WalletPage() {
+  const [tonConnectUI] = useTonConnectUI();
   const { miningStore } = useDispatch<Dispatch>();
   const { account } = useSelector((s: RootState) => s.accountStore);
+  const [ton, setTon] = useState<string | number>();
+  const [mrt, setMrt] = useState<string | number>();
+  const [mrtInGame, setMrtInGame] = useState<string | number>();
+
   const { claimMRT, tonBalance, mrtBalance } = useMaritonToken();
   const nav = useNavigate();
   const claimTokenToTonWallet = async () => {
@@ -78,9 +85,61 @@ export default function WalletPage() {
     if (res) setMrtInGame(undefined);
     return res;
   };
-  const [ton, setTon] = useState<string | number>();
-  const [mrt, setMrt] = useState<string | number>();
-  const [mrtInGame, setMrtInGame] = useState<string | number>();
+  const depositTokenTon = async (transactionId: string) => {
+    const bodyTon = beginCell()
+      .storeUint(0, 32)
+      .storeStringTail(transactionId)
+      .endCell();
+
+    try {
+      const response = await tonConnectUI.sendTransaction({
+        validUntil: Math.floor(Date.now() / 1000) + 600,
+        messages: [
+          {
+            address: DEPOSIT_WALLET,
+            amount: toNano(Number(ton)).toString(),
+            payload: bodyTon.toBoc().toString("base64"),
+          },
+        ],
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const depositTokenMrt = async (transactionId: string) => {
+    const forwardPayload = beginCell()
+      .storeUint(0, 32)
+      .storeStringTail(transactionId)
+      .endCell();
+    const body = beginCell()
+      .storeUint(0xf8a7ea5, 32)
+      .storeUint(0, 64)
+      .storeCoins(toNano(Number(mrt)))
+      .storeAddress(Address.parse(DEPOSIT_WALLET))
+      .storeAddress(Address.parse(DEPOSIT_WALLET))
+      .storeBit(0)
+      .storeCoins(toNano(0.05))
+      .storeBit(1)
+      .storeRef(forwardPayload)
+      .endCell();
+    try {
+      const response = await tonConnectUI.sendTransaction({
+        validUntil: Math.floor(Date.now() / 1000) + 600,
+        messages: [
+          {
+            address: addressMrt,
+            amount: toNano(Number(0.05)).toString(),
+            payload: body.toBoc().toString("base64"),
+          },
+        ],
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <HomeLayout hideNavbar>
       <div className="h-full flex">
@@ -113,7 +172,9 @@ export default function WalletPage() {
               </BaseButton>
               <BaseButton
                 className="flex flex-row justify-center items-center gap-1 bg-b-secondary py-2"
-                onClick={() => {}}
+                onClick={() => {
+                  depositTokenTon("b1ae397c-0d5f-4236-baf4-675b98f0d813");
+                }}
               >
                 <span className="text-sm pt-0.5">Deposit</span>
               </BaseButton>
@@ -139,7 +200,9 @@ export default function WalletPage() {
               </BaseButton>
               <BaseButton
                 className="flex flex-row justify-center items-center gap-1 bg-b-secondary py-2"
-                onClick={() => {}}
+                onClick={() => {
+                  depositTokenMrt("b1ae397c-0d5f-4236-baf4-675b98f0d812");
+                }}
               >
                 <span className="text-sm pt-0.5">Deposit</span>
               </BaseButton>
