30fa322f9dfdbe899b0032a05f6b4a92cb11a3a2
Tho trang <nguyenanhtuan23122000@gmail.com>
2024-07-27 15:46:34 +0700

    continue

--- src/hooks/useAsyncInitialize.ts
+++ src/hooks/useAsyncInitialize.ts
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from 'react';
 
 export function useAsyncInitialize<T>(func: () => Promise<T>, deps: any[] = []) {
