2ad060b7db18bc4ab23f3a457cae0b181e254a12
Thai Bui Minh <=>
2024-07-31 00:23:41 +0700

    fix useTOnClient mainnet testnet

--- src/hooks/useTonClient.ts
+++ src/hooks/useTonClient.ts
@@ -1,12 +1,13 @@
 import { getHttpEndpoint } from "@orbs-network/ton-access";
 import { TonClient } from "@ton/ton";
 import { useAsyncInitialize } from "./useAsyncInitialize";
+import { isTestnet } from "@/config";
 
 export function useTonClient() {
   return useAsyncInitialize(
     async () =>
       new TonClient({
-        endpoint: await getHttpEndpoint({ network: "testnet" }),
+        endpoint: await getHttpEndpoint({ network: isTestnet ? 'testnet' : "mainnet" }),
       })
   );
 }
