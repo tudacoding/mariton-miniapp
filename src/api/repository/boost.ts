422b38243ccfc5e33b447d59b750c842a01c1fff
Thai Bui Minh <=>
2024-08-02 11:36:37 +0700

    continue

--- src/api/repository/boost.ts
+++ src/api/repository/boost.ts
@@ -1,6 +1,7 @@
 import config from "@/config";
 import { EDailyType } from "@/types/models/mining";
 import axios from "axios";
+import qs from "qs";
 const BoostRepository = {
     boostCheckIn: async (account: number, type: EDailyType) => {
         const res = await axios.post(`${config.apiUrl}/daily/daily-boost`, { account, type });
@@ -9,6 +10,18 @@ const BoostRepository = {
     fetchActiveBoosts: async (account: number) => {
         const res = await axios.get(`${config.apiUrl}/daily/active-boost/${account}`);
         return res.data || [];
+    },
+    oneTimeMission: async ({ account, type, status }: { account: number, type: EDailyType, status: string }) => {
+        const res = await axios.post(`${config.apiUrl}/dailies`, {
+            data: {
+                account, type, status
+            }
+        });
+        return res
+    },
+    oneTimeMissionCheck: async (id: number) => {
+        const res = await axios.get(`${config.apiUrl}/daily/ontime-mission-check/${id}`);
+        return res
     }
 };
 export default BoostRepository;
--- src/modules/boost/hooks/useGetDataBoost.tsx
+++ src/modules/boost/hooks/useGetDataBoost.tsx
@@ -4,15 +4,27 @@ import { toast } from "react-toastify";
 import CheckInIcon from "@/assets/icons/CheckinIcon";
 import RichJuniorIcon from "@/assets/icons/RichJuniorIcon";
 import MrtAmbasshador from "@/assets/icons/MrtAmbasshador";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { maritonChannelLink, maritonChatLink, XLink } from "@/config";
 import useCopy from "@/hooks/useCopy";
 import { EDailyType } from "@/types/models/mining";
 import LogoX from "@/assets/icons/LogoX";
 import LogoTele from "@/assets/icons/LogoTele";
+import BoostRepository from "@/api/repository/boost";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
 
 export default function GetDataBoost() {
   const [copy] = useCopy(KEY_MARITON_AMBASSADOR);
+  const { account } = useSelector((s: RootState) => s.accountStore);
+  const handelOneTimeMission = async (type: EDailyType) => {
+    return await BoostRepository.oneTimeMission({
+      type,
+      account: account.id,
+      status: "checking",
+    });
+  };
+  const [id, setId] = useState<number>(0);
   const checkinBoosts: BoostCardProps[] = useMemo(
     () => [
       {
@@ -58,91 +70,100 @@ export default function GetDataBoost() {
     ],
     [copy]
   );
-  const oneTimeBoosts = useMemo(
-    () => [
-      {
-        sortDescription: "Boost +30% mining speed",
-        title: "Follow Mariton on X",
-        description: (
-          <span>
-            X (Follow):{" "}
-            <span
-              className="underline"
-              onClick={() => {
-                window.open(XLink, "_blank");
-              }}
-            >
-              {XLink}
-            </span>
-            <br /> Boost +30% mining speed
+  const oneTimeBoosts = [
+    {
+      sortDescription: "Boost +30% mining speed",
+      title: "Follow Mariton on X",
+      description: (
+        <span>
+          X (Follow):{" "}
+          <span
+            className="underline"
+            onClick={() => {
+              window.open(XLink, "_blank");
+            }}
+          >
+            {XLink}
           </span>
-        ),
-        type: EDailyType.ONE_TIME_FOLLOW_X,
-        icon: <LogoX className="h-10 w-10" />,
-      },
-      {
-        sortDescription: "Boost +30% mining speed",
-        title: "Like and Retweet Mariton's post on X",
-        description: (
-          <>
-            <span className="whitespace-nowrap">Post (Like + Retweet):</span>
-            <p
-              className="underline"
-              onClick={() => {
-                window.open(XLink_2, "_blank");
-              }}
-            >
-              {XLink}
-            </p>
-            Boost +30% mining speed
-          </>
-        ),
-        type: EDailyType.ONE_TIME_LIKE_X,
-        icon: <LogoX className="h-10 w-10" />,
-      },
-      {
-        sortDescription: "Boost +30% mining speed",
-        title: "Join Mariton Community",
-        description: (
-          <span>
-            Chat (Join) :{" "}
-            <span
-              className="underline"
-              onClick={() => {
-                window.open(maritonChatLink, "_blank");
-              }}
-            >
-              {maritonChatLink}
-            </span>
-            <br /> Boost +30% mining speed
+          <br /> Boost +30% mining speed
+        </span>
+      ),
+      type: EDailyType.ONE_TIME_FOLLOW_X,
+      icon: <LogoX className="h-10 w-10" />,
+    },
+    {
+      sortDescription: "Boost +30% mining speed",
+      title: "Like and Retweet Mariton's post on X",
+      description: (
+        <>
+          <span className="whitespace-nowrap">Post (Like + Retweet):</span>
+          <p
+            className="underline"
+            onClick={() => {
+              window.open(XLink_2, "_blank");
+            }}
+          >
+            {XLink}
+          </p>
+          Boost +30% mining speed
+        </>
+      ),
+      type: EDailyType.ONE_TIME_LIKE_X,
+      icon: <LogoX className="h-10 w-10" />,
+    },
+    {
+      sortDescription: "Boost +30% mining speed",
+      title: "Join Mariton Community",
+      description: (
+        <span>
+          Chat (Join) :{" "}
+          <span
+            className="underline"
+            onClick={async () => {
+              window.open(maritonChatLink, "_blank");
+              const res = await handelOneTimeMission(
+                EDailyType.ONE_TIME_JOIN_CHAT_TELE
+              );
+              setId(res.data.data.id);
+            }}
+          >
+            {maritonChatLink}
           </span>
-        ),
-        type: EDailyType.ONE_TIME_JOIN_CHAT_TELE,
-        icon: <LogoTele className="h-9 w-9" />,
-      },
-      {
-        sortDescription: "Boost +30% mining speed",
-        title: "Join Mariton Channel",
-        description: (
-          <span>
-            Channel (Join):{" "}
-            <span
-              className="underline"
-              onClick={() => {
-                window.open(maritonChannelLink, "_blank");
-              }}
-            >
-              {maritonChannelLink}
-            </span>
-            <br /> Boost +30% mining speed
+          <br /> Boost +30% mining speed
+          <span
+            onClick={async () => {
+              const res = await BoostRepository.oneTimeMissionCheck(id);
+              console.log(res);
+            }}
+          >
+            check
           </span>
-        ),
-        type: EDailyType.ONE_TIME_JOIN_CHANNEL_TELE,
-        icon: <LogoTele className="h-9 w-9" />,
-      },
-    ],
-    []
-  );
+        </span>
+      ),
+      type: EDailyType.ONE_TIME_JOIN_CHAT_TELE,
+      icon: <LogoTele className="h-9 w-9" />,
+    },
+    {
+      sortDescription: "Boost +30% mining speed",
+      title: "Join Mariton Channel",
+      description: (
+        <span>
+          Channel (Join):{" "}
+          <span
+            className="underline"
+            onClick={() => {
+              window.open(maritonChannelLink, "_blank");
+            }}
+          >
+            {maritonChannelLink}
+          </span>
+          <br /> Boost +30% mining speed
+        </span>
+      ),
+      type: EDailyType.ONE_TIME_JOIN_CHANNEL_TELE,
+      icon: <LogoTele className="h-9 w-9" />,
+    },
+  ];
   return {
     checkinBoosts,
     oneTimeBoosts,
